7ccd972a681417bfe616519b7e0b4dbfb3a0b7d5
Thiago Nascimento <nascimentoliveira@gmail.com>
2023-01-08 14:49:29 -0300

    feat: hashtag page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useState } from 'react'
+import { useState } from 'react';
+
 
 import SignIn from './pages/SignIn.js';
 import SignUp from './pages/SignUp.js';
 import Timeline from './pages/Timeline.js';
 import UserPosts from './pages/UserPosts.js';
-import HashTag from './pages/Hashtag.js';
+import Hashtag from './pages/Hashtag.js';
 import NotFound from './pages/NotFound.js';
 import './assets/styles/reset.css';
 import './assets/styles/style.css';
@@ -30,7 +31,7 @@ export default function App() {
           <Route path='/signup' element={<SignUp />} />
           <Route path='/timeline' element={<Timeline />} />
           <Route path='/user/:id' element={<UserPosts />} />
-          <Route path='/hashtag/:hashtag' element={<HashTag />} />  
+          <Route path='/hashtag/:hashtag' element={<Hashtag />} />  
           <Route path="*" element={<NotFound />} />
         </Routes>
       </UserContext.Provider>
