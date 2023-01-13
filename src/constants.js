77c402a4cf07d381cbe89e6ed533cf76ffb75daf
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-13 17:49:49 -0300

    feat: deploy back-end

--- src/constants.js
+++ src/constants.js
@@ -1,18 +1,9 @@
-/* const POSTS_ROUTE = 'https://linkr-api-irly.onrender.com/posts';
+const POSTS_ROUTE = 'https://linkr-api-irly.onrender.com/posts';
 const SIGN_IN_ROUTE = 'https://linkr-api-irly.onrender.com/';
 const SIGN_UP_ROUTE = 'https://linkr-api-irly.onrender.com/signup';
 const TIMELINE_ROUTE = 'https://linkr-api-irly.onrender.com/timeline';
 const HASTAGS_ROUTE = 'https://linkr-api-irly.onrender.com/hashtag';
-const URL = 'https://linkr-api-irly.onrender.com'; */
-
-const POSTS_ROUTE = 'http://localhost:4000/posts';
-const SIGN_IN_ROUTE = 'http://localhost:4000/';
-const SIGN_UP_ROUTE = 'http://localhost:4000/signup';
-const TIMELINE_ROUTE = 'http://localhost:4000/timeline';
-const HASTAGS_ROUTE = 'http://localhost:4000/hashtag';
-const POST_DELETE_ROUTE = 'http://localhost:4000/deletepost';
-const URL = 'http://localhost:4000';
-const COMMENTS = 'http://localhost:4000/comments';
+const URL = 'https://linkr-api-irly.onrender.com'; 
 
 const ROUTES = {
   SIGN_UP_ROUTE,
