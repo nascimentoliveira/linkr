0f3683bd3f427d334934a2c5e7fea98ee6dd71d0
Alvaro Galhardo <alvaromgfernandes@gmail.com>
2023-01-13 18:00:49 -0300

    fix: deploy

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,11 @@
-const POSTS_ROUTE = 'https://linkr-api-irly.onrender.com/posts';
-const SIGN_IN_ROUTE = 'https://linkr-api-irly.onrender.com/';
-const SIGN_UP_ROUTE = 'https://linkr-api-irly.onrender.com/signup';
-const TIMELINE_ROUTE = 'https://linkr-api-irly.onrender.com/timeline';
-const HASTAGS_ROUTE = 'https://linkr-api-irly.onrender.com/hashtag';
-const URL = 'https://linkr-api-irly.onrender.com'; 
+const POSTS_ROUTE = "https://linkr-api-irly.onrender.com/posts";
+const SIGN_IN_ROUTE = "https://linkr-api-irly.onrender.com/";
+const SIGN_UP_ROUTE = "https://linkr-api-irly.onrender.com/signup";
+const TIMELINE_ROUTE = "https://linkr-api-irly.onrender.com/timeline";
+const HASTAGS_ROUTE = "https://linkr-api-irly.onrender.com/hashtag";
+const URL = "https://linkr-api-irly.onrender.com";
+const POST_DELETE_ROUTE = "https://linkr-api-irly.onrender.com/deletepost";
+const COMMENTS = "https://linkr-api-irly.onrender.com/comments";
 
 const ROUTES = {
   SIGN_UP_ROUTE,
@@ -13,7 +15,7 @@ const ROUTES = {
   HASTAGS_ROUTE,
   POST_DELETE_ROUTE,
   URL,
-  COMMENTS
+  COMMENTS,
 };
 
 export default ROUTES;
